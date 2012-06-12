5fe4df90db969650a9fa1be89a5ef4d75727d631
Erik Tuerke <tuerke@cbs.mpg.de>
2012-06-12 17:05:55 +0200

    add common.hpp for data

---
+++ isis/data/common.hpp
@@ -0,0 +1,14 @@
+#include <DataStorage/chunk.hpp>
+
+#ifndef _ISIS_PYTHON_DATA_COMMON_HPP
+#define _ISIS_PYTHON_DATA_COMMON_HPP
+
+namespace isis {
+namespace python {
+namespace data {
+void setInitialProperties ( isis::data::Chunk &ch );
+}
+}
+}
+
+#endif // _ISIS_PYTHON_DATA_COMMON_HPP
