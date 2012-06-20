8b26a83c9eea853c78b926a2d7e2ccfa936dbabf
Erik Tuerke <tuerke@cbs.mpg.de>
2012-06-20 10:32:48 +0200

    fixed bug in normalze_values

--- isis/tools/normalize_values.py
+++ isis/tools/normalize_values.py
@@ -4,11 +4,11 @@ from .. import data
 
 def normalize_values( image, min_val, max_val, image_type = 0 ):
 	min_max = image.getMinMax()
-	offset = min_max[0] + min_val
 	scaling = float( max_val - min_val ) / float( min_max[1] - min_max[0] )
+	offset = (min_max[0] * scaling) - min_val
 	if( not image_type ):
 		image_type = data.image_types( image.getMajorTypeID() )
 	arr = image.getArray( image_type )
 	arr *= scaling
-	arr += offset
+	arr -= offset
 	return data.Image( arr, image )
