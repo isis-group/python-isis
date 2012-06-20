45b141a229f37951591f89de3c1693bed7029bbc
Erik Tuerke <tuerke@cbs.mpg.de>
2012-06-20 14:22:29 +0200

    chaged version number to 0.10.2

--- isis/__init__.py
+++ isis/__init__.py
@@ -5,7 +5,7 @@ import tools
 
 __all__=['util','data','tools', 'log', 'getVersion']
 
-VERSION = '0.10.1'
+VERSION = '0.10.2'
 VERSION_AS_LIST = tuple(map(int, VERSION.split('.')))
 
 def getVersion():
